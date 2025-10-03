3a93152fbadab7588fc3da1b4b00bd8b57a5b074
kartheek542 <kartheekdivvela@gmail.com>
2025-10-03 07:26:03 +0530

    added more output variables for network module

--- aws-modules/elk-app/network/outputs.tf
+++ aws-modules/elk-app/network/outputs.tf
@@ -5,3 +5,11 @@ output "vpc_id" {
 output "route_table_id" {
   value = aws_route_table.app_route_table.id
 }
+
+output "public_subnet_id" {
+  value = aws_subnet.public_subnet.id
+}
+
+output "private_subnet_id" {
+  value = aws_subnet.private_subnet.id
+}
