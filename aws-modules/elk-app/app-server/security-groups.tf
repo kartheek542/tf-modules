03dd460b0e5effc1eb097322e5394b3980856eae
kartheek542 <kartheekdivvela@gmail.com>
2025-10-04 12:27:15 +0530

    fixed syntax in cidr range

--- aws-modules/elk-app/app-server/security-groups.tf
+++ aws-modules/elk-app/app-server/security-groups.tf
@@ -33,7 +33,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_public_http" {
   security_group_id = aws_security_group.default_sg.id
-  cidr_ipv4         = "0.0.0.0/"
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = 80
   to_port           = 80
   ip_protocol       = "tcp"
@@ -41,7 +41,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_public_http" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_public_https" {
   security_group_id = aws_security_group.default_sg.id
-  cidr_ipv4         = "0.0.0.0/"
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = 443
   to_port           = 443
   ip_protocol       = "tcp"
