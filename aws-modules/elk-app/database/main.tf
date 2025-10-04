e87fbbc80940f5ed7751ee4e61a07d31d7673bdc
kartheek542 <kartheekdivvela@gmail.com>
2025-10-04 23:47:44 +0530

    created database module

---
+++ aws-modules/elk-app/database/main.tf
@@ -0,0 +1,38 @@
+data "aws_rds_engine_version" "database" {
+  engine  = var.db_engine
+  version = var.db_version
+}
+
+resource "aws_db_parameter_group" "elk-params-group" {
+  name   = "elk-params-group"
+  family = data.aws_rds_engine_version.database.parameter_group_family
+
+  parameter {
+    name  = "log_connections"
+    value = "1"
+  }
+}
+
+resource "aws_db_subnet_group" "subnet_group" {
+  name       = "${terraform.workspace}-elk-db-subnet-group"
+  subnet_ids = var.private_subnet_ids
+
+  tags = {
+    Name = "${terraform.workspace}-elk-db-subnet-group"
+  }
+}
+
+resource "aws_db_instance" "default" {
+  allocated_storage           = 10
+  db_name                     = var.db_name
+  engine                      = data.aws_rds_engine_version.database.engine
+  engine_version              = data.aws_rds_engine_version.database.version
+  port                        = var.db_port
+  username                    = var.master_username
+  manage_master_user_password = true
+  instance_class              = var.instance_class
+  parameter_group_name        = aws_db_parameter_group.elk-params-group.name
+  db_subnet_group_name        = aws_db_subnet_group.subnet_group.name
+  vpc_security_group_ids      = [aws_security_group.database_sg.id]
+  skip_final_snapshot         = true
+}
---
+++ aws-modules/elk-app/database/variables.tf
@@ -0,0 +1,8 @@
+variable "db_name" {}
+variable "db_engine" {}
+variable "db_version" {}
+variable "db_port" {}
+variable "instance_class" { default = "db.t3.micro" }
+variable "master_username" { default = "tf_rds_admin" }
+variable "private_subnet_ids" {}
+variable "public_subnet_id" {}
