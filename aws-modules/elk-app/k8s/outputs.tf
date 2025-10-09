05cfba926aba211244233c24dbadebb2f5bd2712
kartheek542 <kartheekdivvela@gmail.com>
2025-10-09 23:35:34 +0530

    fixed validation issues

--- aws-modules/elk-app/k8s/outputs.tf
+++ aws-modules/elk-app/k8s/outputs.tf
@@ -5,3 +5,7 @@ output "cluster_endpoint" {
 output "cluster_arn" {
   value = aws_eks_cluster.elk_cluster.arn
 }
+
+output "security_group_id" {
+  value = aws_eks_cluster.elk_cluster.vpc_config.security_group_ids[0]
+}
