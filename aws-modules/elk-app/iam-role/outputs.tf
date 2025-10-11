ba17cca66d227f8b36c539bdcbaa1cf8a763df86
kartheek542 <kartheekdivvela@gmail.com>
2025-10-11 23:38:35 +0530

    fixed bugs

--- aws-modules/elk-app/iam-role/outputs.tf
+++ aws-modules/elk-app/iam-role/outputs.tf
@@ -4,6 +4,6 @@ output "role_id" {
 output "role_name" {
   value = aws_iam_role.role.name
 }
-output "instance_profile_id" {
+output "instance_profile_name" {
   value = aws_iam_instance_profile.ec2_instance_profile[*].id
 }
