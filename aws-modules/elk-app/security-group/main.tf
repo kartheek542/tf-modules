c15fd876ba4d1841ae799cecd23b023b90643ea6
kartheek542 <kartheekdivvela@gmail.com>
2025-10-12 00:39:41 +0530

    fixed sg module

--- aws-modules/elk-app/security-group/main.tf
+++ aws-modules/elk-app/security-group/main.tf
@@ -53,7 +53,6 @@ resource "aws_vpc_security_group_ingress_rule" "allow_public_ssh" {
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  count             = terraform.workspace == "prod" ? 1 : 0
   security_group_id = aws_security_group.sg.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
